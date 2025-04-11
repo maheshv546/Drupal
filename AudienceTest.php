0ad1484dcb009e4106540b5583fa49815db18f58
maheshv546 <maheshv546@gmail.com>
2025-04-11 15:50:32 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -3,3 +3,10 @@ protected function refreshThemeConfig(): void {
   \Drupal::service('theme_handler')->refreshInfo();
   \Drupal::service('theme.manager')->resetActiveTheme();
 }
+
+\Drupal::service('theme.manager')->resetActiveTheme();
+\Drupal::service('theme_handler')->refreshInfo();
+\Drupal::service('config.factory')->reset('system.theme.global');
+\Drupal::service('cache.render')->deleteAll();
+\Drupal::service('cache.page')->deleteAll();
+\Drupal::service('cache.dynamic_page_cache')->deleteAll();
