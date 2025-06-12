bf3c5be415b722a74acae3a442e72637c7797e5e
maheshv546 <maheshv546@gmail.com>
2025-06-12 18:07:22 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,8 +1,17 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Using Dart Sass
-
-gulp.task('styles', function () {
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
-});
+function sass(src = options.files.sass, dest = options.paths.build + 'css/', ship = false) {
+  options.sass.style = (ship ? 'compressed' : 'expanded');
+  return gulp.src(src)
+    .pipe($.sassGlob())
+    .pipe($.if(!ship, $.sourcemaps.init()))
+    .pipe(gulpSass(options.sass)
+      .on('error', function(e) {
+        process.stderr.write(new PluginError('sass', e.messageFormatted).toString() + '\n');
+        // if we're not shipping, hide the error by emiting success
+        if (!ship) { this.emit('end'); }
+      })
+    )
+    .pipe(postcss([autoprefixer(AUTOPREFIXER_BROWSERS)]))
+    .pipe($.if(!ship, $.sourcemaps.write()))
+    //.pipe($.if(ship, $.uglify)) // @TODO
+    .pipe(gulp.dest(dest));
+}
