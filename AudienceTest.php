8447bf4dc2f7130f578202f6825fe0a0b5de53d3
maheshv546 <maheshv546@gmail.com>
2025-06-12 18:10:42 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,17 +1,30 @@
+const through = require('through2');
+
+function addTrailingNewline() {
+  return through.obj(function (file, _, cb) {
+    if (file.isBuffer()) {
+      const content = file.contents.toString();
+      if (!content.endsWith('\n')) {
+        file.contents = Buffer.from(content + '\n');
+      }
+    }
+    cb(null, file);
+  });
+}
+
 function sass(src = options.files.sass, dest = options.paths.build + 'css/', ship = false) {
   options.sass.style = (ship ? 'compressed' : 'expanded');
   return gulp.src(src)
     .pipe($.sassGlob())
     .pipe($.if(!ship, $.sourcemaps.init()))
     .pipe(gulpSass(options.sass)
-      .on('error', function(e) {
+      .on('error', function (e) {
         process.stderr.write(new PluginError('sass', e.messageFormatted).toString() + '\n');
-        // if we're not shipping, hide the error by emiting success
         if (!ship) { this.emit('end'); }
       })
     )
     .pipe(postcss([autoprefixer(AUTOPREFIXER_BROWSERS)]))
+    .pipe(addTrailingNewline()) // 👈 Add this line
     .pipe($.if(!ship, $.sourcemaps.write()))
-    //.pipe($.if(ship, $.uglify)) // @TODO
     .pipe(gulp.dest(dest));
 }
