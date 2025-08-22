0122786b537175aae01562278327e67cb4c58f63
maheshv546 <maheshv546@gmail.com>
2025-08-22 18:35:49 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,35 +1,43 @@
-public function testCheckRedirect(): void {
-  $this->getSession()->getDriver()->getClient()->followRedirects(FALSE);
+<?php
 
-  // Create a node.
-  $node = $this->createNode([
-    'type' => 'article',
-    'title' => 'Test Node',
-  ]);
+declare(strict_types=1);
 
-  $canonical_path = '/node/' . $node->id();
+namespace Drupal\Tests\voya_core\Functional;
 
-  // Redirect from /old-path to canonical node.
-  $source = 'old-path';
-  $redirect = Redirect::create([
-    'redirect_source' => ['path' => $source],
-    'redirect_redirect' => ['uri' => 'internal:' . $canonical_path],
-    'status_code' => 301,
-  ]);
-  $redirect->save();
+use Drupal\Tests\BrowserTestBase;
+use Drupal\redirect\Entity\Redirect;
 
-  // Visit the redirect source.
-  $this->getSession()->visit('/' . $source);
+/**
+ * Tests redirect functionality.
+ *
+ * @group custom_module
+ */
+class RedirectTest extends BrowserTestBase {
 
-  // Assert redirect.
-  $status_code = $this->getSession()->getStatusCode();
-  $this->assertEquals(301, $status_code, 'Redirect response returned 301.');
+  protected static $modules = ['node', 'redirect'];
 
-  $response = $this->getSession()->getDriver()->getClient()->getResponse();
-  $headers = $response->getHeaders();
-  $this->assertArrayHasKey('Location', $headers);
-  $redirect_location = $headers['Location'][0];
-  $this->assertEquals($canonical_path, $redirect_location);
+  public function testRedirect(): void {
+    $this->getSession()->getDriver()->getClient()->followRedirects(FALSE);
 
-  $this->getSession()->getDriver()->getClient()->followRedirects(TRUE);
+    // Create a node.
+    $node = $this->createNode(['type' => 'article', 'title' => 'Test Node']);
+    $canonical_path = '/node/' . $node->id();
+
+    // Redirect from /old-path to the node.
+    Redirect::create([
+      'redirect_source' => ['path' => 'old-path'],
+      'redirect_redirect' => ['uri' => 'internal:' . $canonical_path],
+      'status_code' => 301,
+    ])->save();
+
+    // Visit source and assert redirect.
+    $this->getSession()->visit('/old-path');
+    $this->assertEquals(301, $this->getSession()->getStatusCode());
+
+    $location = $this->getSession()->getResponseHeader('Location');
+    $this->assertEquals($canonical_path, parse_url($location, PHP_URL_PATH));
+
+    $this->getSession()->getDriver()->getClient()->followRedirects(TRUE);
+  }
+
 }
