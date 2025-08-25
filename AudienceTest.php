5a7abda37152c31d3fc76227b02b77014545e046
maheshv546 <maheshv546@gmail.com>
2025-08-25 16:58:34 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Drupal\Tests\voya_core\Functional;
+namespace Drupal\Tests\voya_standard\Functional;
 
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
 use Drupal\Tests\BrowserTestBase;
+use Voya\Drupal\Tests\VoyaTestTrait;
 use Drupal\redirect\Entity\Redirect;
 
 /**
@@ -13,30 +16,62 @@ use Drupal\redirect\Entity\Redirect;
  * @group custom_module
  */
 class RedirectTest extends BrowserTestBase {
+  use VoyaTestTrait;
 
-  protected static $modules = ['node', 'redirect'];
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'voya_standard',
+  ];
 
-  public function testRedirect(): void {
+  /**
+   * Checking redirect functionality.
+   */
+  public function testCheckRedirect(): void {
+    // Disable automatic redirect following.
     $this->getSession()->getDriver()->getClient()->followRedirects(FALSE);
 
-    // Create a node.
-    $node = $this->createNode(['type' => 'article', 'title' => 'Test Node']);
-    $canonical_path = '/node/' . $node->id();
+    $this->node1 = $this->drupalCreateNode([
+      'type' => 'article',
+      'title' => 'Article test 1',
+      'status' => NodeInterface::PUBLISHED,
+      'moderation_state' => 'published',
+      'field_post_date' => '2021-12-31T12:00:00',
+    ]);
+    $this->node2 = $this->drupalCreateNode([
+      'type' => 'article',
+      'title' => 'Article test 2',
+      'status' => NodeInterface::PUBLISHED,
+      'field_post_date' => '2022-03-31T12:00:00',
+      'moderation_state' => 'published',
+    ]);
+    $this->node3 = $this->drupalCreateNode([
+      'type' => 'article',
+      'title' => 'Article test 3',
+      'status' => NodeInterface::PUBLISHED,
+      'moderation_state' => 'published',
+      'field_post_date' => '2020-08-31T12:00:00',
+    ]);
 
-    // Redirect from /old-path to the node.
+    $redirect_from_url = '/article/article-test-1';
+    $redirect_to_url = '/article/article-test-2';
+
+    // Create redirect.
     Redirect::create([
-      'redirect_source' => ['path' => 'old-path'],
-      'redirect_redirect' => ['uri' => 'internal:' . $canonical_path],
+      'redirect_source' => ['path' => $redirect_from_url],
+      'redirect_redirect' => ['uri' => 'internal:' . $redirect_to_url],
+      'language' => 'en',
       'status_code' => 301,
     ])->save();
 
-    // Visit source and assert redirect.
-    $this->getSession()->visit('/old-path');
-    $this->assertEquals(301, $this->getSession()->getStatusCode());
+    // Visit the redirect source URL.
+    $this->getSession()->visit(Url::fromUserInput($redirect_from_url)->toString());
 
-    $location = $this->getSession()->getResponseHeader('Location');
-    $this->assertEquals($canonical_path, parse_url($location, PHP_URL_PATH));
+    // Assert the redirect location matches the target URL.
+    $this->assertEquals($redirect_to_url, parse_url($this->getSession()->getResponseHeader('Location'), PHP_URL_PATH));
 
+    // Reset redirect following.
     $this->getSession()->getDriver()->getClient()->followRedirects(TRUE);
   }
 
