8aa144b7a75bc4c1fefc2787f1f6040b05dd225d
maheshv546 <maheshv546@gmail.com>
2025-09-04 15:30:58 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,24 +1,14 @@
-public function resultsPage() {
-  // Ensure to include cacheability for request param we are using!
-  $cache = new CacheableMetadata();
-  $cache->setCacheContexts(['url.query_args:type']);
-
-  $type = strtolower((string) $this->request->getCurrentRequest()->get('type', ''));
-  $type_key = str_replace(" ", "_", $type);
-
-  $asset_classes = $type ? $this->getAssetClasses($type_key) : [];
-  $risk_levels = $this->getRiskLevels();
-  $legal_text = $this->getLegalText();
-
-  $results_page = [
-    '#investor_type' => $type,
-    '#investor_type_key' => $type_key,
-    '#asset_classes' => $asset_classes,
-    '#risk_levels' => $risk_levels,
-    '#legal_text' => $legal_text,
-    '#theme' => 'investor_type_results',
-  ];
-
-  $cache->applyTo($results_page);
-  return $results_page;
-}
+1. All the configuration is stored in codebase.
+2. All the content types are saved as configuration and exported in yml format.
+3. All the content type configuration are in content module inside yml file(modules/custom/module_name/config/install/content_type.yml).
+4. This Configuration can be exported after logging into admin->Development->export->singleitem->select type of configuration->copy the code and create file with the filename specified here.
+5. Each content type has a separate role.
+6. All the custom blocks are created in custom code(modules/custom/module_name/src/plugin/block/blockname.php).
+7. Top level and secondary menus are created in Main Navigation menu.
+8. Voya header block is placed in header region in block layout since it should be available in all the pages.
+9. Voya.com specific files are added inside docroot/sites/www folder.
+10. If change is made for already existing config we need to update that existing file.
+11. Need to check drupal version system requirements for php_version, database etc...
+12. We have 3 environments intg, accp and live environment.
+13. New Relic in pantheon is used to check number of users currently active and any outage in the site pantheon team will check the logs in Relic.
+14. Daily AM task are defined in wikipage og voya github.
