12de0cc2bcac8cf1ebc331e12b1baf8c1c049307
maheshv546 <maheshv546@gmail.com>
2025-09-09 17:01:25 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,26 +1,17 @@
-	1.	All configuration is stored in the codebase.
-	2.	All content types are saved as configuration and exported in YAML (.yml) format.
-	3.	Content type configurations are placed in the module folder:
-modules/custom/module_name/config/install/content_type.yml.
-	4.	Configuration can be exported by logging into Admin → Development → Configuration Synchronization → Export → Single Item.
-	•	Select the type of configuration.
-	•	Copy the exported YAML.
-	•	Create a file with the filename specified in the export screen.
-	5.	Each content type has a separate role associated with it.
-	6.	All custom blocks are created in custom code under:
-modules/custom/module_name/src/Plugin/Block/BlockName.php.
-	7.	Top-level and secondary navigation menus are created in the Main Navigation menu.
-	8.	The Voya Header block is placed in the Header region in Block Layout so that it is available on all pages.
-	9.	Voya.com specific files are stored under:
-docroot/sites/www/.
-	10.	If a change is made to an existing configuration, the existing YAML file must be updated.
-	11.	Always check Drupal system requirements (PHP version, database, etc.) before applying updates.
-	12.	The project has three environments:
-	•	INTG (Integration)
-	•	ACCP (Acceptance)
-	•	LIVE (Production)
-	13.	New Relic on Pantheon is used to monitor:
-	•	Number of active users.
-	•	Site outages.
-	•	Logs are reviewed by the Pantheon team in New Relic.
-	14.	Daily AM tasks are defined in the wiki page of the Voya GitHub repository.
+Drupal voya site code structure
+
+1. .github - has all the workflow actions.
+2. Workflow - All workflow related files are there in this folder.
+3. First call in drupal will go to index.php then it will be routed to all other files.
+4. Core folder has all the drupal core related modules, themes etc..
+5. Modules has two folders one is contrib where the modules are provided from drupal.org and other one custom where the modules are created by us.
+6. sites.php file is used to support multisite architecture.
+
+Resource center site
+
+1. We have usually two content related changes one is Type of Investor updates other one Uploading images. 
+2. First we need to make the changes in accp site for content chnages, once done send for validation through servicenow.
+3. Inorder to update Investror typre content we need to go admin/config/voya-fs/investor_type
+4. Effective date in the document is the one specifies when the chnages to be done on live site. 
+5. Inorder Add images go to Add media->image.
+6. Once the image is added we can send the image path by going to Edit->take the url of the image->Copy the addres and send the image path in servicenow. 
