6e71907594f80a93f7824a17853073b4fc5c675b
maheshv546 <maheshv546@gmail.com>
2025-10-08 21:20:29 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,16 +1,16 @@
 function voya_deep_content_views_pre_view(ViewExecutable $view, string $display_id, array &$args): void {
-  $viewId = $view->id();
-  if ($viewId !== 'deep_content') {
+  if ($view->id() !== 'deep_content' || empty($args[0])) {
     return;
   }
-  // IF RESOURCE NAME IS PASSED, SWAP IT FOR RESOURCE ID.
+
+  /** @var \Drupal\voya_deep_content\TagsHelper $tagsHelper */
   $tagsHelper = \Drupal::service('voya_deep_content.tags_helper');
   $tagsHelper->load();
-  $resourceId = $tagsHelper->tags['RESOURCES'][strtoupper($args[0])];
-  if ($resourceId) {
-    $args[0] = $resourceId;
+
+  $resources = $tagsHelper->tags['RESOURCES'] ?? [];
+  $key = strtoupper($args[0]);
+
+  if (isset($resources[$key])) {
+    $args[0] = $resources[$key];
   }
 }
-
-Exception: Warning: Undefined array key ""
-voya_deep_content_views_pre_view()() (Line: 94)
