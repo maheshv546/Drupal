db4f25a03d32529a98b771c8dd0a79bfe62395ac
maheshv546 <maheshv546@gmail.com>
2025-10-16 12:46:56 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,46 +1,5 @@
-<?php
+The permission 'access block library' comes from
+BlockContentAccessControlHandler::checkCreateAccess()
+in core/modules/block_content/src/BlockContentAccessControlHandler.php.
 
-declare(strict_types=1);
-
-namespace Drupal\mymodule\EventSubscriber;
-
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Drupal\Core\Routing\RoutingEvents;
-use Drupal\Core\Routing\RouteBuildEvent;
-
-/**
- * Forces /admin/content/block to use only 'administer block content' permission.
- */
-final class BlockRouteOverrideSubscriber implements EventSubscriberInterface {
-
-  /**
-   * Replace the route requirement.
-   */
-  public function onAlterRoutes(RouteBuildEvent $event): void {
-    $collection = $event->getRouteCollection();
-    if (!$collection->get('entity.block_content.collection')) {
-      return;
-    }
-
-    $route = $collection->get('entity.block_content.collection');
-
-    // Remove any existing permission requirement completely.
-    $route->setRequirement('_permission', 'administer block content');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents(): array {
-    // Very late priority so it runs AFTER all core/contrib subscribers.
-    $events[RoutingEvents::ALTER][] = ['onAlterRoutes', -1000];
-    return $events;
-  }
-
-}
-
-services:
-  mymodule.block_route_override_subscriber:
-    class: Drupal\mymodule\EventSubscriber\BlockRouteOverrideSubscriber
-    tags:
-      - { name: event_subscriber }
+That’s why _entity_create_any_access:block_content ends up depending on the Access Block Library permission.
