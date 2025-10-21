748fb5ab97174d6baffe637d5cea1ea79f2183a3
maheshv546 <maheshv546@gmail.com>
2025-10-21 18:17:22 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -23,3 +23,19 @@ function voya_blocks_content_block_content_create_access(AccountInterface $accou
     return AccessResult::forbidden();
   }
 }
+
+
+FILE: ...ya_blocks/modules/voya_blocks_content/voya_blocks_content.module
+----------------------------------------------------------------------
+FOUND 2 ERRORS AFFECTING 1 LINE
+----------------------------------------------------------------------
+ 16 | ERROR | Function
+    |       | voya_blocks_content_block_content_create_access() does
+    |       | not have parameter type hint nor @param annotation for
+    |       | its parameter $entity_bundle.
+    |       | (SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingAnyTypeHint)
+ 16 | ERROR | Function
+    |       | voya_blocks_content_block_content_create_access() does
+    |       | not have return type hint nor @return annotation for
+    |       | its return value.
+    |       | (SlevomatCodingStandard.TypeHints.ReturnTypeHint.MissingAnyTypeHint)
