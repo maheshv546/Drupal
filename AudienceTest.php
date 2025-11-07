b5bb45a803a4742dbb3d485010f7e583bdc233fc
maheshv546 <maheshv546@gmail.com>
2025-11-07 21:12:10 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -8,3 +8,5 @@ As a AM person it will be difficult for me to manage all the tasks which include
 6. Snyk issues
 7. Drupal 11 upgrade
 8. Defect issues
+
+Drupal 11 upgrade should be handled by a separate drupal resource as we need to work on this daily follow up on the issue queue look for the MR requets or patches if there is no support from drupal.org going forward we need to remove those modules and write the custom one to achieve what this module swere doing it will be difficult to manage aklong with above task.
