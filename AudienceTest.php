e692bd718f59cb5467157fd8bbd26fa96efa15b8
maheshv546 <maheshv546@gmail.com>
2025-11-07 21:16:59 +0530

    Update AudienceTest.php

--- AudienceTest.php
+++ AudienceTest.php
@@ -1,12 +1,20 @@
-As a AM person it will be difficult for me to manage all the tasks which includes below task mentioned it will be difficult for me to handle alone going forward in AM we dont have any other drupal resources
+Subject: Support Required for Managing Drupal Tasks and Drupal 11 Upgrade
 
-1. Github Action tracking
-2. Log messages
-3. Github issues
-4. npm updates
-5. module and library updates
-6. Snyk issues
-7. Drupal 11 upgrade
-8. Defect issues
+Hi [Recipient’s Name],
 
-Drupal 11 upgrade should be handled by a separate drupal resource as we need to work on this daily follow up on the issue queue look for the MR requets or patches if there is no support from drupal.org going forward we need to remove those modules and write the custom one to achieve what this module swere doing it will be difficult to manage aklong with above task.
+As an AM resource, it will be difficult for me to manage all the ongoing tasks alone. Currently, in the AM team, we don’t have any other Drupal resources to support these activities. The list of tasks includes:
+	1.	GitHub action tracking
+	2.	Log message monitoring
+	3.	GitHub issue tracking
+	4.	NPM updates
+	5.	Module and library updates
+	6.	Snyk issue resolution
+	7.	Drupal 11 upgrade
+	8.	Defect issue management
+
+The Drupal 11 upgrade in particular should be handled by a dedicated Drupal resource. This task requires daily follow-up on issue queues, monitoring merge requests or patches, and identifying unsupported modules. If certain modules are not maintained on Drupal.org, we may need to remove them and develop custom solutions to maintain functionality.
+
+Managing all these tasks along with the upgrade effort will be difficult without additional support. Hence, I recommend assigning a dedicated Drupal resource to handle the Drupal 11 upgrade activities.
+
+Regards,
+[Your Name]
